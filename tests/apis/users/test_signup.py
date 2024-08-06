89b38261a478959f63b60bfd77a732ddd5469a5f
JungHoon99 <greenjm1@nate.com>
2024-08-07 00:51:07 +0900

    [test] user signup api

--- tests/apis/users/test_signup.py
+++ tests/apis/users/test_signup.py
@@ -6,5 +6,14 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 @pytest.mark.asyncio
 async def test_signup(client: AsyncClient, session: AsyncSession):
-    response = await client.post("/users/signup")
+    body = {
+        "email": "test12@gmail.com",
+        "password": "1q2w3e4r!",
+        "username": "testuser",
+    }
+
+    response = await client.post("/users/signup", json=body)
+
     assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["email"] == body["email"]
+    assert response.json()["username"] == body["username"]
