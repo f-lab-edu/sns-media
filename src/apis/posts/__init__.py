7c5abfeec22ff9a508fbc4b57a09360f3ccc80d9
JungHoon99 <greenjm1@nate.com>
2024-08-06 22:59:24 +0900

    post url modify

--- src/apis/posts/__init__.py
+++ src/apis/posts/__init__.py
@@ -2,18 +2,18 @@ from fastapi import APIRouter, status
 
 from src.apis.posts import create_post, get_post, get_posts
 
-post_router = APIRouter(tags=["posts"])
+post_router = APIRouter(prefix="/posts", tags=["posts"])
 
 post_router.add_api_route(
     methods=["POST"],
-    path="/posts",
+    path="",
     endpoint=create_post.handler,
     response_model=create_post.CreatePostResponse,
     status_code=status.HTTP_201_CREATED,
 )
 post_router.add_api_route(
     methods=["GET"],
-    path="/posts",
+    path="",
     endpoint=get_posts.handler,
     response_model=list[get_posts.GetPostResponse],
     status_code=status.HTTP_200_OK,
@@ -21,7 +21,7 @@ post_router.add_api_route(
 
 post_router.add_api_route(
     methods=["GET"],
-    path="/posts/{post_id}",
+    path="/{post_id}",
     endpoint=get_post.handler,
     response_model=get_post.GetPostResponse,
     status_code=status.HTTP_200_OK,
