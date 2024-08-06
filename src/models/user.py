7c2aaba23be4454310efe88359ea59c0969aca71
JungHoon99 <greenjm1@nate.com>
2024-08-06 22:09:15 +0900

    user password 단순화

--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,5 @@
 import uuid
 
-import bcrypt
 from sqlmodel import Field, SQLModel
 
 
@@ -8,14 +7,4 @@ class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     email: str = Field(unique=True, nullable=False)
     username: str = Field(nullable=False)
-    hashed_password: str
-
-    @property
-    def password(self):
-        return self.hashed_password
-
-    @password.setter
-    def password(self, password):
-        salt = bcrypt.gensalt()
-        hashed_password = str(bcrypt.hashpw(password.encode("utf-8"), salt))
-        self.hashed_password = hashed_password
+    password: str = Field(nullable=False)
