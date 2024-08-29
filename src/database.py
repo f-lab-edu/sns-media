417ff2504709c83b1e8011fe71a81f8789744546
JungHoon99 <greenjm1@nate.com>
2024-08-30 01:21:16 +0900

    [feat] locust를 사용해 get post 성능 테스트

--- src/database.py
+++ src/database.py
@@ -1,13 +1,21 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import SQLModel, pool
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src import config
 
 engine = create_async_engine(
     url=config.db.url,
     echo=config.db.echo,
-    poolclass=pool.StaticPool,
+    pool_size=20,
+    max_overflow=30,
+    pool_pre_ping=True,
+    pool_recycle=500,
 )
+
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def create_db_and_tables() -> None:
